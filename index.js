3f5170826dcd6d9d8d7c1af480d53009c3b49404
Tate-T <tatjana.tarasovych@gmail.com>
2022-05-30 16:15:50 +0300

    program parce

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const contacts = require("./contacts");
 
 const path = require("path");
 
-const argv = require("yargs").argv;
+const { Command } = require("commander");
+const program = new Command();
+program
+    .option("-a, --action <type>", "choose action")
+    .option("-i, --id <type>", "user id")
+    .option("-n, --name <type>", "user name")
+    .option("-e, --email <type>", "user email")
+    .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
     switch (action) {
